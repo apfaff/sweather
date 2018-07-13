91a58d0d3b9dd8b19654ceadf19a16a9b1b4cc29
apfaff <alex@pemedia.de>
2018-07-13 17:03:03 +0200

    Remove weather-report endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -2,23 +2,13 @@ require('dotenv').config()
 require('./data/mongodb')
 
 const restify = require('restify')
-const { requestCurrentWeatherDataByZip } = require('./util/weatherApi')
 const { register, unregister } = require('./data/resolver/notification')
-
-const respond = async (req, res, next) => {
-  const weather = await requestCurrentWeatherDataByZip(req.query.zip, req.query.country)
-  res.send(weather)
-  return next()
-}
 
 const server = restify.createServer()
 server.use(restify.plugins.fullResponse())
 server.use(restify.plugins.gzipResponse())
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
-
-server.get('/weather-report', respond)
-server.head('/weather-report', respond)
 
 server.post('/register', register)
 server.post('/unregister/:token', unregister)
